1efed51b7807a0478e9fcccd9f721f91ce39f70f
KasperHoulind <32668404+KasperHoulind@users.noreply.github.com>
2018-09-17 10:08:16 +0200

    Initial commit

---
+++ mathlibTest/mathlibTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using mathlib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mathlibTest
+{
+    [TestClass]
+    public class mathlibTest
+    {
+        HejMatematik m = new HejMatematik();
+        [TestMethod]
+        public void addTest()
+        {
+        
+
+            Assert.IsTrue(m.Add(2, 3) == 5);
+        }
+        [TestMethod]
+        public void addTestNegativeNumber()
+
+        {
+            
+            Assert.AreEqual(m.Add(-3, -4), -7);
+        }
+    }
+
+}
